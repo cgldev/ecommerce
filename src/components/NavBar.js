3c4f556efa06bd75068d0b1ffebb7f8e51eded70
cglmdq <5015332+cglmdq@users.noreply.github.com>
2020-10-30 16:02:45 -0300

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
-// menu
 import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div>
+      <h1 className="titulo">BIENVENIDOS</h1>
       <ul>
         <li>Item 1</li>
         <li>Item 2</li>
