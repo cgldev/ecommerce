333482e1b852cb2f410e98b32715ebbf74d0962e
cglmdq <5015332+cglmdq@users.noreply.github.com>
2020-10-30 15:45:38 -0300

    componente navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,16 @@
 // menu
+import React from "react";
 
-const menu = false;
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
