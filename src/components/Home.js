70afeed08d90b6bb3656680d8028c980cecfb048
cglmdq <5015332+cglmdq@users.noreply.github.com>
2020-11-10 12:14:52 -0300

    boton

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import "./Home.css";
 
 const Home = ({ greeting }) => {
+  const addToContador = () => {
+    setContador(contador + 1);
+  };
+  const subToContador = () => {
+    setContador(contador - 1);
+  };
+  const [contador, setContador] = useState(0);
   return (
     <section className="hero">
       <h2>{greeting}</h2>
       <h3>Sitio ecommerce demo realizado con React.js</h3>
+      <div>
+        <button onClick={addToContador}>Sumar</button>
+        <button onClick={subToContador}>Restar</button>
+        {contador}
+      </div>
     </section>
   );
 };
