7333b378ddb9555065671ef16e3aa36f18b31e28
cglmdq <5015332+cglmdq@users.noreply.github.com>
2020-12-04 12:13:39 -0300

    importar cartContext

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./CartIcon.css";
 import Cart from "./shopping-cart.png";
 import { useContext } from "react";
+import { cartContext } from "../../context/cartContext.js";
 
 const CartIcon = () => {
   const cartCantidad = useContext(cartContext);
