244050b2b6322218411428de6e16de6233732362
cglmdq <5015332+cglmdq@users.noreply.github.com>
2021-01-04 00:03:24 -0300

    componente Contacto

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,7 +1,26 @@
+import "./Contacto.css";
+
 export default function Contacto() {
   return (
     <>
-      <h1>CONTACTO</h1>
+      <section id="contact">
+        <div class="form-wrapper">
+          <header class="form-head">
+            <h2>Contactanos</h2>
+          </header>
+          <form>
+            <div class="name-form">
+              <label for="name">Nombre:</label>
+              <input id="name" type="text" name="name" required />
+            </div>
+            <div class="email-form">
+              <label for="email">Correo :</label>
+              <input id="email" type="email" name="email" required />
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </section>
     </>
   );
 }
