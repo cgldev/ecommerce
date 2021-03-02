597b176b9e2b434a2af944ff4a08e95c98828083
cglmdq <5015332+cglmdq@users.noreply.github.com>
2021-03-02 09:07:54 -0300

    estilos cart y list

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,12 +3,10 @@ import "./ItemList.css";
 
 export default function ItemList({ productos }) {
   return (
-    <div className="itemList">
-      <div className="items">
-        {productos.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </div>
+    <div className="items">
+      {productos.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
     </div>
   );
 }
